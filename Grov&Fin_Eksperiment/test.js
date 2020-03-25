ce9240b2820088d11365c3a1322cb682b783f2bb
Joachim Demuth <jdemuth18@gmail.com>
2020-03-25 15:19:29 +0100

    background color

--- Grov&Fin_Eksperiment/test.js
+++ Grov&Fin_Eksperiment/test.js
@@ -1,7 +1,10 @@
-var ball = document.querySelector(".ball");
-var garden = document.querySelector(".garden");
-var output = document.querySelector(".output");
-console.log(ball);
+var ball = document.getElementById("ball");
+var boxvest = document.getElementById("boxvest");
+var boxnord = document.getElementById("boxnord");
+var boxoest = document.getElementById("boxoest");
+var boxsyd = document.getElementById("boxsyd");
+var garden = document.getElementById("garden");
+var output = document.getElementById("output");
 
 var maxX = garden.clientWidth - ball.clientWidth;
 var maxY = garden.clientHeight - ball.clientHeight;
@@ -12,6 +15,14 @@ function handleOrientation(event) {
 
   output.innerHTML = "beta : " + x + "\n";
   output.innerHTML += "gamma: " + y + "\n";
+  output.innerHTML += "Ball Top: " + ball.style.top + "\n";
+  output.innerHTML += "Ball Left: " + ball.style.left + "\n";
+  output.innerHTML += "Box Vest: " + boxvest.style.width + "\n";
+
+  // width: 150px;
+  // height: 70px;
+  // left: 40px;
+  // top: 350px;
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
@@ -31,6 +42,15 @@ function handleOrientation(event) {
   // It center the positioning point to the center of the ball
   ball.style.top = (maxY * y) / 180 - 10 + "px";
   ball.style.left = (maxX * x) / 180 - 10 + "px";
+  let topMeasure = parseInt(ball.style.top, 10);
+
+  if (topMeasure >= 350 && topMeasure <= 420) {
+    boxvest.style.backgroundColor = "green";
+  } else {
+    boxvest.style.backgroundColor = "white";
+  }
+
+  console.log(topMeasure);
 }
 
 window.addEventListener("deviceorientation", handleOrientation);
