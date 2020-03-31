81e31a64ce8f6d988c2d27aff9d2c6d04db4b9c0
nicolainyboe <57439100+nicolainyboe@users.noreply.github.com>
2020-03-31 12:54:50 +0200

    Ryddet op i test.js
    Kode er rykket til moja_mqtt script

--- Gross-Fine-Motor-Skills/Grov&Fin_Eksperiment/test.js
+++ Gross-Fine-Motor-Skills/Grov&Fin_Eksperiment/test.js
@@ -69,59 +69,59 @@ function handleOrientation(event) {
   //ball.style.top = (maxY * y) / 180 - 10 + "px";
   //ball.style.left = (maxX * x) / 180 - 10 + "px";
 
-  let topMeasure = parseInt(ball.style.top, 10);
-  console.log(topMeasure);
-  let leftMeasure = parseInt(ball.style.left, 10);
-  //VEST
+  // let topMeasure = parseInt(ball.style.top, 10);
+  // console.log(topMeasure);
+  // let leftMeasure = parseInt(ball.style.left, 10);
+  // //VEST
 
-  if (
-    topMeasure >= 350 &&
-    topMeasure <= 460 &&
-    leftMeasure >= 40 &&
-    leftMeasure <= 230
-  ) {
-    boksenDuRammer = 1;
-    ifRightFunctionEvent(1);
-  } else {
-    boxvest.style.backgroundColor = "white";
-  }
+  // if (
+  //   topMeasure >= 350 &&
+  //   topMeasure <= 460 &&
+  //   leftMeasure >= 40 &&
+  //   leftMeasure <= 230
+  // ) {
+  //   boksenDuRammer = 1;
+  //   ifRightFunctionEvent(1);
+  // } else {
+  //   boxvest.style.backgroundColor = "white";
+  // }
 
-  //NORD width: 150px;height: 70px;left: 410px;top: 40px;
-  if (
-    topMeasure >= 40 &&
-    topMeasure <= 150 &&
-    leftMeasure >= 410 &&
-    leftMeasure <= 600
-  ) {
-    boksenDuRammer = 2;
-    ifRightFunctionEvent(2);
-  } else {
-    boxnord.style.backgroundColor = "white";
-  }
-  // OEST width: 150px;height: 70px;left: 760px;top: 350px;
-  if (
-    topMeasure >= 350 &&
-    topMeasure <= 460 &&
-    leftMeasure >= 760 &&
-    leftMeasure <= 950
-  ) {
-    boksenDuRammer = 3;
-    ifRightFunctionEvent(3);
-  } else {
-    boxoest.style.backgroundColor = "white";
-  }
-  //SYD left: 410px; top: 640px; height: 70px; width:150px;
-  if (
-    topMeasure >= 640 &&
-    topMeasure <= 750 &&
-    leftMeasure >= 410 &&
-    leftMeasure <= 600
-  ) {
-    boksenDuRammer = 4;
-    ifRightFunctionEvent(4);
-  } else {
-    boxsyd.style.backgroundColor = "white";
-  }
+  // //NORD width: 150px;height: 70px;left: 410px;top: 40px;
+  // if (
+  //   topMeasure >= 40 &&
+  //   topMeasure <= 150 &&
+  //   leftMeasure >= 410 &&
+  //   leftMeasure <= 600
+  // ) {
+  //   boksenDuRammer = 2;
+  //   ifRightFunctionEvent(2);
+  // } else {
+  //   boxnord.style.backgroundColor = "white";
+  // }
+  // // OEST width: 150px;height: 70px;left: 760px;top: 350px;
+  // if (
+  //   topMeasure >= 350 &&
+  //   topMeasure <= 460 &&
+  //   leftMeasure >= 760 &&
+  //   leftMeasure <= 950
+  // ) {
+  //   boksenDuRammer = 3;
+  //   ifRightFunctionEvent(3);
+  // } else {
+  //   boxoest.style.backgroundColor = "white";
+  // }
+  // //SYD left: 410px; top: 640px; height: 70px; width:150px;
+  // if (
+  //   topMeasure >= 640 &&
+  //   topMeasure <= 750 &&
+  //   leftMeasure >= 410 &&
+  //   leftMeasure <= 600
+  // ) {
+  //   boksenDuRammer = 4;
+  //   ifRightFunctionEvent(4);
+  // } else {
+  //   boxsyd.style.backgroundColor = "white";
+  // }
 }
 
 window.addEventListener("deviceorientation", handleOrientation);
