3109aa9b661d8395eb18e1e17e61721ee8bb62ec
NicolaiNyboe <nnyboe93@gmail.com>
2020-04-18 14:42:28 +0200

    Update moja_mqtt2020.js

--- Gross-Fine-Motor-Skills/moja_mqtt2020.js
+++ Gross-Fine-Motor-Skills/moja_mqtt2020.js
@@ -135,6 +135,13 @@ function ifRightSequence(e) {
           "Antal rigtige felter = " + (count + 1) + "/8";
         document.getElementById("boxsyd").style.backgroundColor = "green";
       }
+      if (count + 1 == sequence.length) {
+        setTimeout(500);
+
+        window.location.href = "countdown.html";
+
+        console.log("nu vil jeg videre");
+      }
     }
   }
 }
